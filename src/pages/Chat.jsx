149b79e32d18f0068e798e18b1c91f404f196b25
DevShohruhmirzo <shohruhmirzonumonjonov@gmail.com>
2024-10-12 13:57:22 +0300

    chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./Chat.css"
 
 function Chat() {
   return (
     <div>
       <h1>Chat</h1>
+      <div className="chat">
+        <div className="chat__right"></div>
+        <div className="chat__center"></div>
+        <div className="chat__left">
+          <div className="chat__box">
+            <div className="chat__card">
+              <img src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=2312" alt="" />
+              <span>
+                <h2>Proyektor</h2>
+                <p>Hozzir aktiv</p>
+              </span>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
     </div>
   )
 }
